3fcc1c4a49258a1523cf43d139b813d3b82bdf14
fluffysocks <fluffysocksrocks@proton.me>
2025-10-21 18:59:34 -0400

    i forgot the plus sign like a fool

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { page } from '$app/state'
+</script>
+
+<style>
+    * {
+        color: red;
+    }
+</style>
+
+<h1>{page.status}</h1>
+<p>{page.error?.message} ... :&lpar; this is very sad</p>
